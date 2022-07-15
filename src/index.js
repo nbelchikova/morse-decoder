9b5a6566ea618fa9067e402b1a483d44600d775d
nbelchikova <n.belchikova@yandex.ru>
2022-07-15 11:48:35 +0300

    docs:change js

--- src/index.js
+++ src/index.js
@@ -35,11 +35,64 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********':  ' ',
 };
+let result='';
+    let a='';
+    let tire= "11";
+    let point="10";
+    let probel='**********';
+    for (key in MORSE_TABLE) {
+        for (let i=0; i<key.length; i++) {
+            if (key[i]==='-') {
+                a=a+tire;
+            } else if (key[i]==='.') {
+                a=a+point;
+            }
+           
+              
+        }
+        
+        if (MORSE_TABLE[key].length===10){
+            MORSE_TABLE[a]=MORSE_TABLE[key]
+        } else {
+            let t=a.padStart(10, "0");
+            MORSE_TABLE[t]=MORSE_TABLE[key];
 
+        }
+         
+        a='';
+        
+         
+        
+    }
+    console.log(MORSE_TABLE);
+    
 function decode(expr) {
-    // write your solution here
-}
+    // wte your solution here
+    result='';
+    for (let j=0; j<expr.length; j=j+10) {
+        
+        let ten= expr.slice(j, j+10);
+        
+        for (key in MORSE_TABLE) {
+            
+            if (ten===key) {
+                
+                result=result +MORSE_TABLE[key];
+            } 
+            
+        }
+        
+        
+           
+    } 
+    return result;
+     
+    }
+    
+    
+
 
 module.exports = {
     decode
